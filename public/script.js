c56fcd74e32662013bad63fae6d17adc67d89059
Alejandro Martinez <alejandromtel@gmail.com>
2022-01-11 22:48:08 -0400

    Cambi parametros de inicializacion de Peer con servidor de heroku

--- public/script.js
+++ public/script.js
@@ -3,8 +3,11 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-    host: '/',
-    port: '3001'
+    /*host: '/',
+    port: '3001'*/
+    secure: true, 
+                host: 'speakfast.herokuapp.com', 
+                port: 443
 })
 
 const myVideo = document.createElement('video')
